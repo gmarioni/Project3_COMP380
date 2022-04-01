62ee70382b483f862710e1562b69744996437df6
Daniel Notari <notari.daniel@gmail.com>
2022-03-31 21:12:05 -0700

    updates to db class basic working connection and query

--- db.py
+++ db.py
@@ -1,33 +1,43 @@
+from xmlrpc.client import Boolean
 import mysql.connector
-from mysql.connector import Error
 
-class db:
-    username = "root"
-    password = "temp"
-    def create_connection(host_name, user_name, user_password):
+class Database:
+    """Database MySQL"""
+    def __init__(self,username="root",password="temp",host="localhost",port=8806,database="pms"):
+        self.connection = None
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
+        print(username)
 
-        connection = None
-
+    def create_connection(self):
+        """Create Connection for MySQL"""
+        self.connection = None
         try:
-
-            connection = mysql.connector.connect(
-
-                host=host_name,
-
-                user=user_name,
-
-                passwd=user_password
-
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                user=self.username,
+                passwd=self.password,
+                port=self.port,
+                database=self.database
             )
-
+            print(self.connection.is_connected())
             print("Connection to MySQL DB successful")
+        except mysql.connector.Error as err:
+            print(f"The error '{err}' occurred")
 
-        except Error as e:
-
-            print(f"The error '{e}' occurred")
-
-
-        return connection
-
-
-    connection = create_connection("localhost", username, "")
+    def is_connected(self) -> bool:
+        """Return if Database is Connected"""
+        return self.connection.is_connected()
+    
+    def run_query(self,query):
+        """Run Query"""
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+        return cursor.fetchall()
+    
+    def run_sp(self,sp_name,sp_parameters=None):
+        """Run a Store Proceedure"""
+        return None
